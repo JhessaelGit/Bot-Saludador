f26e136afcc94dfbcf1f1031eb31c2b8f9a3be8b
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-18 14:16:37 -0400

    test: Se agrego un nuevo test para el saludo en la Noche en ingles

--- src/saludadorIngles.test.js
+++ src/saludadorIngles.test.js
@@ -6,6 +6,9 @@ describe("Saludador Ingles", ()=>{
     it("Deberia saludar en la Tarde en forma joven",()=>{
         expect(saludarIngles("Jhessael","M",17,13)).toEqual("Good Afternoon Young Jhessael");
     })
+    it("Deberia saludar en la Noche en forma joven",()=>{
+        expect(saludarIngles("Jhessael","M",17,22)).toEqual("Good Evening Young Jhessael");
+    })
     it("deberia saludar en para la tarde a un senior", ()=>{
         expect(saludarIngles("Jhessael","M",22,13)).toEqual("Good Afternoon Mister Jhessael");
     });
