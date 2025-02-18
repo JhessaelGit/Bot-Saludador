6ceb3736a0b560845f77670d51b2587da262d2ab
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-18 14:11:59 -0400

    test: Se agrego un nuevo test para el saludo en la maniana en ingles

--- src/saludadorIngles.test.js
+++ src/saludadorIngles.test.js
@@ -1,6 +1,10 @@
 import saludarIngles from "./saludadorIngles";
 describe("Saludador Ingles", ()=>{
-    it("deberia saludar en ingles", ()=>{
+    it("Deberia saludar en la maniana a en forma joven",()=>{
+        expect(saludarIngles("Jhessael","M",17,10)).toEqual("Good Morning Young Jhessael");
+    })
+    
+    it("deberia saludar en para la tarde a un senior", ()=>{
         expect(saludarIngles("Jhessael","M",22,13)).toEqual("Good Afternoon Mister Jhessael");
     });
 })
