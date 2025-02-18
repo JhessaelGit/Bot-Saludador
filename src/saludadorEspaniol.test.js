2ef3dec9e28c68fdf9c0d5f358b2352ea2a44937
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-18 14:36:07 -0400

    test: Se agregaron los test para el Saludo espaniol

--- src/saludadorEspaniol.test.js
+++ src/saludadorEspaniol.test.js
@@ -1,9 +1,42 @@
-import saludadorEspaniol from "./saludadorEspaniol";
-describe("Saludar en Ingles", () => {
-  it("Debe saludar en espaniol con los datos agregados", () => {
-    expect(saludadorEspaniol("Jhessael","M",22,13)).toEqual("Buenas tardes Señor Jhessael");
-
+import saludarEspaniol from "./saludadorEspaniol";
+describe("Saludador Español Masculino", ()=>{
+  it("Deberia saludar en la maniana a en forma joven",()=>{
+      expect(saludarEspaniol("Jhessael","M",17,10)).toEqual("Buenos dias Joven Jhessael");
+  })
+  it("Deberia saludar en la Tarde en forma joven",()=>{
+      expect(saludarEspaniol("Jhessael","M",17,13)).toEqual("Buenas tardes Joven Jhessael");
+  })
+  it("Deberia saludar en la Noche en forma joven",()=>{
+      expect(saludarEspaniol("Jhessael","M",17,22)).toEqual("Buenas noches Joven Jhessael");
+  })
+  it("deberia saludar para la maniana en forma de senior", ()=>{
+      expect(saludarEspaniol("Jhessael","M",22,8)).toEqual("Buenos dias Señor Jhessael");
   });
-});
+  it("deberia saludar para la tarde en forma de senior", ()=>{
+      expect(saludarEspaniol("Jhessael","M",22,13)).toEqual("Buenas tardes Señor Jhessael");
+  });
+  it("deberia saludar para la noche en forma de senior", ()=>{
+      expect(saludarEspaniol("Jhessael","M",22,22)).toEqual("Buenas noches Señor Jhessael");
+  });
+})
 
-
+describe("Saludador Español Femenino", ()=>{
+  it("Deberia saludar en la maniana a en forma seniorita",()=>{
+      expect(saludarEspaniol("Maria","F",17,10)).toEqual("Buenos dias Señorita Maria");
+  })
+  it("Deberia saludar en la Tarde en forma joven",()=>{
+      expect(saludarEspaniol("Maria","F",17,13)).toEqual("Buenas tardes Señorita Maria");
+  })
+  it("Deberia saludar en la Noche en forma joven",()=>{
+      expect(saludarEspaniol("Maria","F",17,22)).toEqual("Buenas noches Señorita Maria");
+  })
+  it("deberia saludar para la maniana en forma de senior", ()=>{
+      expect(saludarEspaniol("Maria","F",22,8)).toEqual("Buenos dias Señora Maria");
+  });
+  it("deberia saludar para la tarde en forma de senior", ()=>{
+      expect(saludarEspaniol("Maria","F",22,13)).toEqual("Buenas tardes Señora Maria");
+  });
+  it("deberia saludar para la noche en forma de senior", ()=>{
+      expect(saludarEspaniol("Maria","F",22,22)).toEqual("Buenas noches Señora Maria");
+  });
+})
--- src/saludadorIngles.test.js
+++ src/saludadorIngles.test.js
@@ -9,13 +9,13 @@ describe("Saludador Ingles Masculino", ()=>{
     it("Deberia saludar en la Noche en forma joven",()=>{
         expect(saludarIngles("Jhessael","M",17,22)).toEqual("Good Evening Young Jhessael");
     })
-    it("deberia saludar en para la maniana en forma de senior", ()=>{
+    it("deberia saludar para la maniana en forma de senior", ()=>{
         expect(saludarIngles("Jhessael","M",22,8)).toEqual("Good Morning Mister Jhessael");
     });
-    it("deberia saludar en para la tarde en forma de senior", ()=>{
+    it("deberia saludar para la tarde en forma de senior", ()=>{
         expect(saludarIngles("Jhessael","M",22,13)).toEqual("Good Afternoon Mister Jhessael");
     });
-    it("deberia saludar en para la noche en forma de senior", ()=>{
+    it("deberia saludar para la noche en forma de senior", ()=>{
         expect(saludarIngles("Jhessael","M",22,22)).toEqual("Good Evening Mister Jhessael");
     });
 })
@@ -30,13 +30,13 @@ describe("Saludador Ingles Femenino", ()=>{
     it("Deberia saludar en la Noche en forma joven",()=>{
         expect(saludarIngles("Maria","F",17,22)).toEqual("Good Evening Miss Maria");
     })
-    it("deberia saludar en para la maniana en forma de senior", ()=>{
+    it("deberia saludar para la maniana en forma de senior", ()=>{
         expect(saludarIngles("Maria","F",22,8)).toEqual("Good Morning Misses Maria");
     });
-    it("deberia saludar en para la tarde en forma de senior", ()=>{
+    it("deberia saludar para la tarde en forma de senior", ()=>{
         expect(saludarIngles("Maria","F",22,13)).toEqual("Good Afternoon Misses Maria");
     });
-    it("deberia saludar en para la noche en forma de senior", ()=>{
+    it("deberia saludar para la noche en forma de senior", ()=>{
         expect(saludarIngles("Maria","F",22,22)).toEqual("Good Evening Misses Maria");
     });
 })
